194b632eba43f45095433770d9f109c60b2fe0f1
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2024-12-27 07:08:29 +0530

    added SectionList for testing;

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,17 +1,45 @@
 import ViewScreen from "@/components/ViewScreen";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { SectionList, Text, View } from "react-native";
 import BackButton from "@/components/BackButton";
+import DayItem from "@/components/DayItem";
 
 export default function Settings() {
+  const settingsSections = ["General"]
+  const DATA = [
+    {
+      title: 'Main dishes',
+      data: ['Pizza', 'Burger', 'Risotto'],
+    },
+    {
+      title: 'Sides',
+      data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+    },
+    {
+      title: 'Drinks',
+      data: ['Water', 'Coke', 'Beer'],
+    },
+    {
+      title: 'Desserts',
+      data: ['Cheese Cake', 'Ice Cream'],
+    },
+  ];
+  
   return (
     <ViewScreen header='Settings Header' footer='Settings Footer'>
       
-      <Text>Settings</Text>
-      <Text>Settings</Text>
-      <Text>Settings</Text>
-      <Text>Settings</Text>
-      <Text>Settings</Text>
+      <SectionList
+        keyExtractor={(item, index) => item + index}
+        sections={DATA}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({section: {title}}) => (
+          <Text style={{fontWeight: 'bold'}}>{title}</Text>
+        )}
+         />
 
     </ViewScreen>
   );
